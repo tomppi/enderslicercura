c16253c9c9948494666bb6fb2d6122764029562b
tomppi <60854909+tomppi@users.noreply.github.com>
2026-07-18 13:22:01 +0300

    Use AGP legacy JNI packaging for executable engine

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,9 @@ android {
         resources {
             excludes += setOf("/META-INF/{AL2.0,LGPL2.1}")
         }
+        jniLibs {
+            useLegacyPackaging = true
+        }
     }
 }
 
