aba04e0edf9d50a192c57ee362c5a64905f68bea
tomppi <60854909+tomppi@users.noreply.github.com>
2026-07-18 23:54:18 +0300

    Bump printer compatibility build to 0.4.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.tomppi.enderslicer"
         minSdk = 29
         targetSdk = 36
-        versionCode = 17
-        versionName = "0.4.1-dev"
+        versionCode = 18
+        versionName = "0.4.2-dev"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
