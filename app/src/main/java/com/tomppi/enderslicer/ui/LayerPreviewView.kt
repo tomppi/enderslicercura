975c7779b7dae45b598c5d5a2e598c946e9f5db0
tomppi <60854909+tomppi@users.noreply.github.com>
2026-07-18 23:01:57 +0300

    Use Row and Column scoped weight modifiers correctly

--- app/src/main/java/com/tomppi/enderslicer/ui/LayerPreviewView.kt
+++ app/src/main/java/com/tomppi/enderslicer/ui/LayerPreviewView.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
