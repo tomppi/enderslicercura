ffafc457102771c11dced421047cae1acda89604
tomppi <60854909+tomppi@users.noreply.github.com>
2026-07-18 13:48:45 +0300

    Expose error log export in the app

--- app/src/main/java/com/tomppi/enderslicer/MainActivity.kt
+++ app/src/main/java/com/tomppi/enderslicer/MainActivity.kt
@@ -4,16 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.tomppi.enderslicer.ui.EnderSlicerApp
 import com.tomppi.enderslicer.ui.EnderSlicerTheme
+import com.tomppi.enderslicer.ui.ErrorLogExporter
+import com.tomppi.enderslicer.ui.MainViewModel
 
 class MainActivity : ComponentActivity() {
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             EnderSlicerTheme {
-                EnderSlicerApp()
+                Box(modifier = Modifier.fillMaxSize()) {
+                    EnderSlicerApp(viewModel)
+                    ErrorLogExporter(
+                        viewModel = viewModel,
+                        modifier = Modifier
+                            .align(Alignment.BottomEnd)
+                            .padding(16.dp),
+                    )
+                }
             }
         }
     }
