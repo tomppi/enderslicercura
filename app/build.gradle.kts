16fd26ec43fd8e079e0743bae7d9774da9174fe0
tomppi <60854909+tomppi@users.noreply.github.com>
2026-07-18 14:53:05 +0300

    Bump development APK to 0.2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.tomppi.enderslicer"
         minSdk = 29
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.2.3-dev"
+        versionCode = 7
+        versionName = "0.2.4-dev"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
