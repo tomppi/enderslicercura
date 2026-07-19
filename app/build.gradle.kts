eae74b1f1c19df6fd6fb11250d11dfcf75ab23fb
tomppi <60854909+tomppi@users.noreply.github.com>
2026-07-19 13:45:01 +0300

    Bump imported-profile parity build to 0.5.7-dev

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.tomppi.enderslicer"
         minSdk = 29
         targetSdk = 36
-        versionCode = 25
-        versionName = "0.5.6-dev"
+        versionCode = 26
+        versionName = "0.5.7-dev"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
