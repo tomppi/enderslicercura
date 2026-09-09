7ed71ecd3e4ff62fd8dfa572935a422bc0faf5bb
tomppi <tomppi_94@hotmail.com>
2026-09-09 09:35:03 +0300

    fix(ci): install OpenSSL into lib/ (--libdir=lib; the linux target defaults to lib64)

--- scripts/build-prusa-engine-android-3.sh
+++ scripts/build-prusa-engine-android-3.sh
@@ -110,6 +110,10 @@ rep(root / 'deps/+OpenSSL/OpenSSL.cmake',
     '    CONFIGURE_COMMAND ${_conf_cmd} ${_cross_arch}',
     '    CONFIGURE_COMMAND env "CC=${_ndk_bin}/${_ndk_cc_name}" "CXX=${_ndk_bin}/${_ndk_cxx_name}" ${_conf_cmd} ${_cross_arch}',
     'openssl: clang wrapper')
+rep(root / 'deps/+OpenSSL/OpenSSL.cmake',
+    '        "--prefix=${PROJECT_NAME}_DEP_INSTALL_PREFIX"\n        ${_cross_comp_prefix_line}',
+    '        "--prefix=${PROJECT_NAME}_DEP_INSTALL_PREFIX"\n        --libdir=lib\n        ${_cross_comp_prefix_line}',
+    'openssl: libdir lib')
 PY
 
 # OpenSSL's android configuration still looks for NDK <triple>-gcc names;
