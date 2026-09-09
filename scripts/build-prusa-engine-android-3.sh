8075e5d1c4f74b1c237311928f1b7e83b2bb6245
tomppi <tomppi_94@hotmail.com>
2026-09-09 08:30:57 +0300

    fix(ci): use NDK clang wrappers for autotools deps (CC/CXX export + sysroot); final patch set

--- scripts/build-prusa-engine-android-3.sh
+++ scripts/build-prusa-engine-android-3.sh
@@ -47,10 +47,7 @@ rep(root / 'deps/CMakeLists.txt',
     'if (UNIX AND NOT ANDROID)\n    # On UNIX systems (including Apple) ZLIB should be available\n    list(APPEND SYSTEM_PROVIDED_PACKAGES ZLIB)\nendif ()',
     'deps: ZLIB is source-built on Android')
 
-
-
-# gmplib.org / mpfr.org are unreachable from GitHub runners; mirror on ftp.gnu.org
-# (identical tarballs, same hashes).
+# gmplib.org / mpfr.org are unreachable from GitHub runners; mirror on ftp.gnu.org.
 rep(root / 'deps/+GMP/GMP.cmake',
     'URL https://gmplib.org/download/gmp/gmp-6.2.1.tar.bz2',
     'URL https://ftp.gnu.org/gnu/gmp/gmp-6.2.1.tar.bz2',
@@ -63,52 +60,45 @@ rep(root / 'deps/+MPFR/MPFR.cmake',
 # GMP/MPFR autotools builds default to the host compiler; target Android via the
 # NDK clang wrapper + the android-* host triplet.
 rep(root / 'deps/+GMP/GMP.cmake',
-    '        set(_cfg_cmd env "CFLAGS=${_gmp_ccflags}" "CXXFLAGS=${_gmp_ccflags}" ./configure ${_cross_compile_arg} --enable-shared=no --enable-cxx=yes --enable-static=yes "--prefix=${${PROJECT_NAME}_DEP_INSTALL_PREFIX}" ${_gmp_build_tgt})',
-    '        set(_cfg_cmd env "CC=${CMAKE_C_COMPILER}" "CXX=${CMAKE_CXX_COMPILER}" "CFLAGS=${_gmp_ccflags}" "CXXFLAGS=${_gmp_ccflags}" ./configure ${_cross_compile_arg} --enable-shared=no --enable-cxx=yes --enable-static=yes "--prefix=${${PROJECT_NAME}_DEP_INSTALL_PREFIX}" ${_gmp_build_tgt})',
-    'gmp: android clang compiler')
-rep(root / 'deps/+GMP/GMP.cmake',
     '    set(_cross_compile_arg "")\n    if (APPLE)',
-    '    set(_cross_compile_arg "")\n    if (ANDROID)\n        if (ANDROID_ABI STREQUAL "arm64-v8a")\n            set(_cross_compile_arg --host=aarch64-linux-android)\n        elseif (ANDROID_ABI STREQUAL "x86_64")\n            set(_cross_compile_arg --host=x86_64-linux-android)\n        endif ()\n        set(_gmp_build_tgt "")\n    elseif (APPLE)',
+    '    set(_cross_compile_arg "")\n    if (ANDROID)\n        if (ANDROID_ABI STREQUAL "arm64-v8a")\n            set(_cross_compile_arg --host=aarch64-linux-android)\n        elseif (ANDROID_ABI STREQUAL "x86_64")\n            set(_cross_compile_arg --host=x86_64-linux-android)\n        endif ()\n        set(_gmp_build_tgt "")\n        get_filename_component(_ndk_bin ${CMAKE_C_COMPILER} DIRECTORY)\n        if (ANDROID_ABI STREQUAL "arm64-v8a")\n            set(_ndk_cc_name aarch64-linux-android24-clang)\n            set(_ndk_cxx_name aarch64-linux-android24-clang++)\n        elseif (ANDROID_ABI STREQUAL "x86_64")\n            set(_ndk_cc_name x86_64-linux-android24-clang)\n            set(_ndk_cxx_name x86_64-linux-android24-clang++)\n        endif ()\n    elseif (APPLE)',
     'gmp: android host triplet')
 rep(root / 'deps/+GMP/GMP.cmake',
     '    elseif (CMAKE_CROSSCOMPILING)\n        # TOOLCHAIN_PREFIX should be defined in the toolchain file\n        set(_cross_compile_arg --host=${TOOLCHAIN_PREFIX})',
     '    elseif (CMAKE_CROSSCOMPILING AND NOT ANDROID)\n        # TOOLCHAIN_PREFIX should be defined in the toolchain file\n        set(_cross_compile_arg --host=${TOOLCHAIN_PREFIX})',
     'gmp: keep android host triplet')
 rep(root / 'deps/+GMP/GMP.cmake',
-    '    set(_cross_compile_arg "")\n    if (ANDROID)\n        if (ANDROID_ABI STREQUAL "arm64-v8a")\n            set(_cross_compile_arg --host=aarch64-linux-android)\n        elseif (ANDROID_ABI STREQUAL "x86_64")\n            set(_cross_compile_arg --host=x86_64-linux-android)\n        endif ()\n        set(_gmp_build_tgt "")\n    elseif (APPLE)',
-    '    set(_cross_compile_arg "")\n    if (ANDROID)\n        if (ANDROID_ABI STREQUAL "arm64-v8a")\n            set(_cross_compile_arg --host=aarch64-linux-android)\n        elseif (ANDROID_ABI STREQUAL "x86_64")\n            set(_cross_compile_arg --host=x86_64-linux-android)\n        endif ()\n        set(_gmp_build_tgt "")\n        if (ANDROID_ABI STREQUAL "arm64-v8a")\n            set(_gmp_ccflags "${_gmp_ccflags} --target=aarch64-linux-android24 --sysroot=${CMAKE_SYSROOT}")\n        elseif (ANDROID_ABI STREQUAL "x86_64")\n            set(_gmp_ccflags "${_gmp_ccflags} --target=x86_64-linux-android24 --sysroot=${CMAKE_SYSROOT}")\n        endif ()\n    elseif (APPLE)',
-    'gmp: android clang target flag')
-rep(root / 'deps/+MPFR/MPFR.cmake',
-    "                 CFLAGS='${_gmp_ccflags}' \\\n                 CXXFLAGS='${_gmp_ccflags}' \\",
-    "                 CC='${CMAKE_C_COMPILER}' \\\n                 CFLAGS='${_gmp_ccflags}' \\\n                 CXXFLAGS='${_gmp_ccflags}' \\\n",
-    'mpfr: android clang compiler')
+        '    set(_cross_compile_arg "")\n    if (ANDROID)\n        if (ANDROID_ABI STREQUAL "arm64-v8a")\n            set(_cross_compile_arg --host=aarch64-linux-android)\n        elseif (ANDROID_ABI STREQUAL "x86_64")\n            set(_cross_compile_arg --host=x86_64-linux-android)\n        endif ()\n        set(_gmp_build_tgt "")\n        get_filename_component(_ndk_bin ${CMAKE_C_COMPILER} DIRECTORY)\n        if (ANDROID_ABI STREQUAL "arm64-v8a")\n            set(_ndk_cc_name aarch64-linux-android24-clang)\n            set(_ndk_cxx_name aarch64-linux-android24-clang++)\n        elseif (ANDROID_ABI STREQUAL "x86_64")\n            set(_ndk_cc_name x86_64-linux-android24-clang)\n            set(_ndk_cxx_name x86_64-linux-android24-clang++)\n        endif ()\n    elseif (APPLE)',
+    '    set(_cross_compile_arg "")\n    if (ANDROID)\n        if (ANDROID_ABI STREQUAL "arm64-v8a")\n            set(_cross_compile_arg --host=aarch64-linux-android)\n        elseif (ANDROID_ABI STREQUAL "x86_64")\n            set(_cross_compile_arg --host=x86_64-linux-android)\n        endif ()\n        set(_gmp_build_tgt "")\n        get_filename_component(_ndk_bin ${CMAKE_C_COMPILER} DIRECTORY)\n        if (ANDROID_ABI STREQUAL "arm64-v8a")\n            set(_ndk_cc_name aarch64-linux-android24-clang)\n            set(_ndk_cxx_name aarch64-linux-android24-clang++)\n        elseif (ANDROID_ABI STREQUAL "x86_64")\n            set(_ndk_cc_name x86_64-linux-android24-clang)\n            set(_ndk_cxx_name x86_64-linux-android24-clang++)\n        endif ()\n        if (ANDROID_ABI STREQUAL "arm64-v8a")\n            set(_gmp_ccflags "${_gmp_ccflags} --target=aarch64-linux-android24 --sysroot=${CMAKE_SYSROOT}")\n        elseif (ANDROID_ABI STREQUAL "x86_64")\n            set(_gmp_ccflags "${_gmp_ccflags} --target=x86_64-linux-android24 --sysroot=${CMAKE_SYSROOT}")\n        endif ()\n    elseif (APPLE)',
+    'gmp: android target flags')
+rep(root / 'deps/+GMP/GMP.cmake',
+    '        set(_cfg_cmd env "CFLAGS=${_gmp_ccflags}" "CXXFLAGS=${_gmp_ccflags}" ./configure',
+    '        set(_cfg_cmd env "CC=${_ndk_bin}/${_ndk_cc_name}" "CXX=${_ndk_bin}/${_ndk_cxx_name}" "CFLAGS=${_gmp_ccflags}" "CXXFLAGS=${_gmp_ccflags}" ./configure',
+    'gmp: android clang wrapper')
+
 rep(root / 'deps/+MPFR/MPFR.cmake',
     '    if (EMSCRIPTEN)\n        set(_cross_compile_arg --host=wasm32)\n    endif ()',
-    '    if (ANDROID)\n        if (ANDROID_ABI STREQUAL "arm64-v8a")\n            set(_cross_compile_arg --host=aarch64-linux-android)\n        elseif (ANDROID_ABI STREQUAL "x86_64")\n            set(_cross_compile_arg --host=x86_64-linux-android)\n        endif ()\n    elseif (EMSCRIPTEN)\n        set(_cross_compile_arg --host=wasm32)\n    endif ()',
-    'mpfr: android host triplet')
-rep(root / 'deps/+MPFR/MPFR.cmake',
-    '    if (ANDROID)\n        if (ANDROID_ABI STREQUAL "arm64-v8a")\n            set(_cross_compile_arg --host=aarch64-linux-android)\n        elseif (ANDROID_ABI STREQUAL "x86_64")\n            set(_cross_compile_arg --host=x86_64-linux-android)\n        endif ()\n    elseif (EMSCRIPTEN)',
-    '    if (ANDROID)\n        if (ANDROID_ABI STREQUAL "arm64-v8a")\n            set(_cross_compile_arg --host=aarch64-linux-android)\n        elseif (ANDROID_ABI STREQUAL "x86_64")\n            set(_cross_compile_arg --host=x86_64-linux-android)\n        endif ()\n        if (ANDROID_ABI STREQUAL "arm64-v8a")\n            set(_gmp_ccflags "${_gmp_ccflags} --target=aarch64-linux-android24 --sysroot=${CMAKE_SYSROOT}")\n        elseif (ANDROID_ABI STREQUAL "x86_64")\n            set(_gmp_ccflags "${_gmp_ccflags} --target=x86_64-linux-android24 --sysroot=${CMAKE_SYSROOT}")\n        endif ()\n    elseif (EMSCRIPTEN)',
-    'mpfr android clang target flag')
+    '    if (ANDROID)\n        if (ANDROID_ABI STREQUAL "arm64-v8a")\n            set(_cross_compile_arg --host=aarch64-linux-android)\n        elseif (ANDROID_ABI STREQUAL "x86_64")\n            set(_cross_compile_arg --host=x86_64-linux-android)\n        endif ()\n        get_filename_component(_ndk_bin ${CMAKE_C_COMPILER} DIRECTORY)\n        if (ANDROID_ABI STREQUAL "arm64-v8a")\n            set(_ndk_cc_name aarch64-linux-android24-clang)\n        elseif (ANDROID_ABI STREQUAL "x86_64")\n            set(_ndk_cc_name x86_64-linux-android24-clang)\n        endif ()\n        if (ANDROID_ABI STREQUAL "arm64-v8a")\n            set(_gmp_ccflags "${_gmp_ccflags} --target=aarch64-linux-android24 --sysroot=${CMAKE_SYSROOT}")\n        elseif (ANDROID_ABI STREQUAL "x86_64")\n            set(_gmp_ccflags "${_gmp_ccflags} --target=x86_64-linux-android24 --sysroot=${CMAKE_SYSROOT}")\n        endif ()\n    elseif (EMSCRIPTEN)',
+    'mpfr: android host triplet + target flags')
 rep(root / 'deps/+Imath/Imath.cmake',
     '-DLIBDEFLATE_BUILD_SHARED_LIB=OFF\n        -DLIBDEFLATE_BUILD_GZIP=OFF',
     '-DLIBDEFLATE_BUILD_SHARED_LIB=OFF\n        -DLIBDEFLATE_BUILD_GZIP=OFF\n        -DBUILD_TESTING=OFF',
     'imath: tests off')
+
 # OpenSSL ships no generic CMake config: its ./Configure must target android-*.
 rep(root / 'deps/+OpenSSL/OpenSSL.cmake',
     'set(_conf_cmd "./config")\nset(_cross_arch "")\nset(_cross_comp_prefix_line "")\nset(_apple_target_flags "")',
     'set(_conf_cmd "./config")\nset(_cross_arch "")\nset(_cross_comp_prefix_line "")\nset(_apple_target_flags "")\nset(_openssl_tgt "")',
     'openssl: tgt var')
 rep(root / 'deps/+OpenSSL/OpenSSL.cmake',
-    '    CONFIGURE_COMMAND ${_conf_cmd} ${_cross_arch}',
-    '    CONFIGURE_COMMAND env "CC=${CMAKE_C_COMPILER} ${_openssl_tgt} --sysroot=${CMAKE_SYSROOT}" "CXX=${CMAKE_CXX_COMPILER} ${_openssl_tgt} --sysroot=${CMAKE_SYSROOT}" ${_conf_cmd} ${_cross_arch}',
-    'openssl: clang env')
-rep(root / 'deps/+OpenSSL/OpenSSL.cmake',
     'elseif (CMAKE_CROSSCOMPILING)',
-    'elseif (ANDROID)\n    set(_conf_cmd "./Configure")\n    if (ANDROID_ABI STREQUAL "arm64-v8a")\n        set(_cross_arch "linux-aarch64")\n        set(_openssl_tgt "--target=aarch64-linux-android24")\n    elseif (ANDROID_ABI STREQUAL "x86_64")\n        set(_cross_arch "linux-x86_64")\n        set(_openssl_tgt "--target=x86_64-linux-android24")\n    else ()\n        message(FATAL_ERROR "OpenSSL: unsupported Android ABI: ${ANDROID_ABI}")\n    endif ()\nelseif (CMAKE_CROSSCOMPILING)',
+    'elseif (ANDROID)\n    set(_conf_cmd "./Configure")\n    if (ANDROID_ABI STREQUAL "arm64-v8a")\n        set(_cross_arch "linux-aarch64")\n        set(_openssl_tgt "--target=aarch64-linux-android24")\n    elseif (ANDROID_ABI STREQUAL "x86_64")\n        set(_cross_arch "linux-x86_64")\n        set(_openssl_tgt "--target=x86_64-linux-android24")\n    else ()\n        message(FATAL_ERROR "OpenSSL: unsupported Android ABI: ${ANDROID_ABI}")\n    endif ()\n    get_filename_component(_ndk_bin ${CMAKE_C_COMPILER} DIRECTORY)\n    if (ANDROID_ABI STREQUAL "arm64-v8a")\n        set(_ndk_cc_name aarch64-linux-android24-clang)\n        set(_ndk_cxx_name aarch64-linux-android24-clang++)\n    elseif (ANDROID_ABI STREQUAL "x86_64")\n        set(_ndk_cc_name x86_64-linux-android24-clang)\n        set(_ndk_cxx_name x86_64-linux-android24-clang++)\n    endif ()\nelseif (CMAKE_CROSSCOMPILING)',
     'openssl: android configure target')
+rep(root / 'deps/+OpenSSL/OpenSSL.cmake',
+    '    CONFIGURE_COMMAND ${_conf_cmd} ${_cross_arch}',
+    '    CONFIGURE_COMMAND env "CC=${_ndk_bin}/${_ndk_cc_name}" "CXX=${_ndk_bin}/${_ndk_cxx_name}" ${_conf_cmd} ${_cross_arch}',
+    'openssl: clang wrapper')
 PY
-
 
 # OpenSSL's android configuration still looks for NDK <triple>-gcc names;
 # the NDK ships clang wrappers only, so provide the classic symlinks.
@@ -120,6 +110,17 @@ for TRIPLE in aarch64-linux-android x86_64-linux-android; do
   ln -sf clang "$NDKBIN/$TRIPLE-gcc"
   ln -sf clang++ "$NDKBIN/$TRIPLE-g++"
 done
+
+# Autotools-based deps (GMP/MPFR/OpenSSL/Lua) inherit the NDK wrappers through
+# the exported CC/CXX; the NDK clang wrapper configures target + sysroot itself.
+TOOLBIN=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin
+if [ "$ABI" = "arm64-v8a" ]; then
+  export CC=$TOOLBIN/aarch64-linux-android24-clang
+  export CXX=$TOOLBIN/aarch64-linux-android24-clang++
+elif [ "$ABI" = "x86_64" ]; then
+  export CC=$TOOLBIN/x86_64-linux-android24-clang
+  export CXX=$TOOLBIN/x86_64-linux-android24-clang++
+fi
 
 step "[3/5] dependency bundle (deps/ ExternalProject chain)"
 mkdir -p "$PREFIX"
